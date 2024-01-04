dc654c6325659fe58f1804da3a21ab42b3adc247
JiwonYun <85991535+yuunstory@users.noreply.github.com>
2024-01-04 10:09:25 +0900

    feat: 비밀번호 암호화 추가

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,14 +1,21 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto"); //암호화
 const dotenv = require("dotenv");
 dotenv.config();
 
 const join = (req, res) => {
     const { email, password } = req.body;
+    const sql = "INSERT INTO users (email, password, salt) VALUES(?, ?, ?)";
 
-    const sql = "INSERT INTO users (email, password) VALUES(?, ?)";
-    const values = [email, password];
+    // 회원가입 시 비밀번호를 암호화해서 암호화된 비밀번호와 salt 값을 DB에 같이 저장
+    const salt = crypto.randomBytes(10).toString("base64");
+    const hashPassword = crypto.pbkdf2Sync(password, salt, 10000, 10, "sha512").toString("base64");
+
+    // 로그인 시, 이메일 & 비밀번호(날 것) => salt값 꺼내서 비밀번호 암호화 해보고 => DB에 저장된 비밀번호와 비교
+
+    const values = [email, hashPassword, salt];
     conn.query(sql, values, (err, results) => {
         if (err) {
             console.log(err);

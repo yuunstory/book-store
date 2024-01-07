506484301eed038dc964b5c44ebfa4dabddef3da
JiwonYun <85991535+yuunstory@users.noreply.github.com>
2024-01-07 21:09:30 +0900

    feat: 카테고리별, 신간별, 전체 도서 조회 기능 수정 및 추가

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,35 +1,36 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
-// (요약된) 전체 도서 조회
+// (카테고리별, 신간 여부별) 전체 도서 조회
 const getAllBooks = (req, res) => {
-    const { categoryId } = req.query;
+    const { categoryId, newBook } = req.query;
 
-    if (categoryId) {
-        // 카테고리별 도서 조회
-        const sql = "SELECT * FROM books WHERE category_id = ?";
-        conn.query(sql, categoryId, (err, results) => {
-            if (err) {
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
+    let sql = "SELECT * FROM books";
+    let values = [];
 
-            if (results.length) {
-                return res.status(StatusCodes.OK).json(results);
-            } else {
-                return res.status(StatusCodes.NOT_FOUND).end();
-            }
-        });
-    } else {
-        // 전체 도서 조회
-        const sql = "SELECT * FROM books";
-        conn.query(sql, (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            return res.status(StatusCodes.OK).json(results);
-        });
+    if (categoryId && newBook) {
+        sql += " WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 3 MONTH) AND NOW()";
+        values = [categoryId, newBook];
+    } else if (categoryId) {
+        sql += " WHERE category_id = ?";
+        values = [categoryId];
+    } else if (newBook) {
+        sql += " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+        values = [newBook];
     }
+
+    conn.query(sql, values, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+
+        if (results.length) {
+            return res.status(StatusCodes.OK).json(results);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+    });
 };
 
 // 개별 도서 조회

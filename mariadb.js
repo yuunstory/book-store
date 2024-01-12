dd84c8ce4aeb962bc2bd76950ce30b8c2904c93e
JiwonYun <85991535+yuunstory@users.noreply.github.com>
2024-01-12 12:19:20 +0900

    update: db 모듈에 db  연결 설정 추가와 비동기 db  연결로 수정

--- mariadb.js
+++ mariadb.js
@@ -1,34 +1,27 @@
-// const mariadb = require("mysql2/promise");
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// const connection = mariadb.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: process.env.DB_PASSWORD,
-//   database: "Bookstore",
-//   dateStrings: true,
-// });
-
-// module.exports = connection;
-
 const mariadb = require("mysql2/promise");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = mariadb.createPool({
-  host: "localhost",
-  user: "root",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "Bookstore",
+  database: process.env.DB_DATABASE,
   dateStrings: true,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  namedPlaceholders: true,
 });
 
 const getConnection = async () => {
-  return await pool.getConnection();
+  try {
+    const connection = await pool.getConnection();
+    return connection;
+  } catch (err) {
+    console.error("Database Connection Error: ", err);
+    throw err; // 오류를 다시 던져 상위 스코프에서 처리할 수 있도록 함
+  }
 };
 
 module.exports = { getConnection };

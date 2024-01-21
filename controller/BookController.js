14436b55ce0a50b4ab06e3f225da8b57cc958b2a
JiwonYun <85991535+yuunstory@users.noreply.github.com>
2024-01-21 21:18:19 +0900

    feat: jwt 토큰으로 회원 인증 추가와 sql query문 named placeholder로 수정

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,75 +1,74 @@
-const conn = require("../mariadb");
+const pool = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
-// (카테고리별, 신간 여부별) 전체 도서 조회
-const getAllBooks = (req, res) => {
-  let { categoryId, newBook, limit, currentPage } = req.query;
-  // let { category_id : categoryId, newBook, limit, current_page : currentPage } = req.query;
-  // let { category_id : categoryId, newBook, limit, current_page : currentPage } = req.query;
-  const offset = (currentPage - 1) * limit;
+/** (카테고리별, 신간 여부별) 전체 도서 조회  */
+const getAllBooks = async (req, res) => {
+  try {
+    const connection = await pool.getConnection();
+    let { categoryId, newBook, limit, currentPage } = req.query;
 
-  // const basicSql = ''
+    const offset = (currentPage - 1) * limit;
 
-  let sql = `SELECT *,
-                (SELECT count(*) FROM likes WHERE books.id = liked_book_id) AS likes
-            FROM books`;
-  let values = [];
-  if (categoryId && newBook) {
-    // const categoryFilteredSql = basicSql + ``;
-    sql += " WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 3 MONTH) AND NOW()";
-    values = [categoryId];
-  } else if (categoryId) {
-    sql += " WHERE category_id = ?";
-    values = [categoryId];
-  } else if (newBook) {
-    sql += " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
-  }
-
-  sql += " LIMIT ? OFFSET ?";
-  values = [...values, parseInt(limit), offset];
-
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(StatusCodes.BAD_REQUEST).end();
+    let sql = `SELECT *,
+                  (SELECT count(*) FROM likes WHERE books.id = liked_book_id) AS likes
+              FROM books`;
+    let values = [];
+    if (categoryId && newBook) {
+      sql += " WHERE category_id = :categoryId AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 3 MONTH) AND NOW()";
+      values = { categoryId: categoryId };
+    } else if (categoryId) {
+      sql += " WHERE category_id = ?";
+      values = [categoryId];
+    } else if (newBook) {
+      sql += " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
     }
+
+    sql += " LIMIT :limit OFFSET :offset";
+    values[limit] = parseInt(limit);
+    values[offset] = offset;
+
+    const [results] = await connection.query(sql, values);
 
     if (results.length) {
       return res.status(StatusCodes.OK).json(results);
     } else {
       return res.status(StatusCodes.NOT_FOUND).end();
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-// 개별 도서 조회
-const getBookById = (req, res) => {
-  const { userId } = req.body;
-  const bookId = req.params.id;
+/** 개별 도서 조회 */
+const getBookById = async (req, res) => {
+  try {
+    const connection = await pool.getConnection();
 
-  const sql = `SELECT * , 
+    const { userId } = req.body;
+    const bookId = req.params.id;
+
+    const sql = `SELECT * , 
                     (SELECT count(*) FROM likes WHERE books.id = liked_book_id) AS likes, 
-                    (SELECT EXISTS (SELECT * FROM likes WHERE user_id = ? AND liked_book_id = ?)) AS liked 
+                    (SELECT EXISTS (SELECT * FROM likes WHERE user_id = :userId AND liked_book_id = :likedBookId)) AS liked 
                 FROM books 
                 LEFT JOIN category 
                 ON books.category_id = category.category_id  
-                WHERE books.id =?`;
-  const values = [userId, bookId, bookId];
+                WHERE books.id = :bookId`;
+    const values = { userId: userId, likedBookId: bookId, bookId: bookId };
 
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(StatusCodes.NOT_FOUND).json({
-        message: "존재하지 않는 도서입니다.",
-      });
-    }
+    const [results] = await connection.query(sql, values);
 
     if (results[0]) {
       return res.status(StatusCodes.OK).json(results[0]);
     } else {
       return res.status(StatusCodes.NOT_FOUND).end();
     }
-  });
+  } catch (err) {
+    console.log(err);
+    return res.status(StatusCodes.NOT_FOUND).json({
+      message: "존재하지 않는 도서입니다.",
+    });
+  }
 };
 
 module.exports = { getAllBooks, getBookById };

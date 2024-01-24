9f7086532781d10a8aae800f2d49c6347d66c91c
JiwonYun <85991535+yuunstory@users.noreply.github.com>
2024-01-24 17:17:03 +0900

    refactor: LikeController.js와 models/like.js 좋아요 API 코드분리

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -1,36 +1,40 @@
 const checkAuthorization = require("../middlewares/auth");
-const pool = require("../mariadb");
+const { addLikeToDB, removeLikeFromDB } = require("../models/like");
 const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
 
 /** 좋아요 추가 */
 const addLike = async (req, res) => {
-    const connection = await pool.getConnection();
-
-    const bookId = req.params.id; //bookId
+    const bookId = req.params.id;
 
     const authorization = checkAuthorization(req, res);
+
     if (authorization instanceof jwt.TokenExpiredError) {
         return res.status(StatusCodes.UNAUTHORIZED).json({
             message: "로그인 토큰이 만료되었습니다. 다시 로그인하세요.",
         });
-    } else if (authorization instanceof jwt.JsonWebTokenError) {
+    }
+
+    if (authorization instanceof jwt.JsonWebTokenError) {
+        console.log(jwt.JsonWebTokenError);
         return res.status(StatusCodes.BAD_REQUEST).json({
             message: "잘못된 토큰입니다.",
         });
-    } else {
-        const sql = "INSERT INTO likes (user_id, liked_book_id) VALUES (:userId, :bookId);";
-        const values = { userId: authorization.id, bookId: bookId };
-        const [results] = await connection.query(sql, values);
-
-        return res.status(StatusCodes.OK).json(results);
     }
+
+    if (authorization == null) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            message: "로그인이 필요합니다.",
+        });
+    }
+
+    const insertedResults = await addLikeToDB({ bookId, authorization });
+
+    return res.status(StatusCodes.OK).json(insertedResults);
 };
 
 /** 좋아요 삭제 */
 const removeLike = async (req, res) => {
-    const connection = await pool.getConnection();
-
     const bookId = req.params.id;
 
     const authorization = checkAuthorization(req, res);
@@ -38,16 +42,23 @@ const removeLike = async (req, res) => {
         return res.status(StatusCodes.UNAUTHORIZED).json({
             message: "로그인 토큰이 만료되었습니다. 다시 로그인하세요.",
         });
-    } else if (authorization instanceof jwt.JsonWebTokenError) {
+    }
+
+    if (authorization instanceof jwt.JsonWebTokenError) {
+        console.log(jwt.JsonWebTokenError);
         return res.status(StatusCodes.BAD_REQUEST).json({
             message: "잘못된 토큰입니다.",
         });
-    } else {
-        const sql = "DELETE FROM likes WHERE user_id = :userId AND liked_book_id = :bookId";
-        const values = { userId: authorization.id, bookId: bookId };
-        const [results] = await connection.query(sql, values);
-        return res.status(StatusCodes.OK).json(results);
     }
+
+    if (authorization == null) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            message: "로그인이 필요합니다.",
+        });
+    }
+
+    const removedResults = await removeLikeFromDB({ bookId, authorization });
+    return res.status(StatusCodes.OK).json(removedResults);
 };
 
 module.exports = { addLike, removeLike };
---
+++ models/like.js
@@ -0,0 +1,24 @@
+const pool = require("../mariadb");
+
+const addLikeToDB = async ({ bookId, authorization }) => {
+    const connection = await pool.getConnection();
+
+    const insertLikeSql = "INSERT INTO likes (user_id, liked_book_id) VALUES (:userId, :bookId);";
+    const values = { userId: authorization.id, bookId };
+
+    const [results] = await connection.query(insertLikeSql, values);
+
+    return results;
+};
+
+const removeLikeFromDB = async ({ bookId, authorization }) => {
+    const connection = await pool.getConnection();
+
+    const deleteLikeSql = "DELETE FROM likes WHERE user_id = :userId AND liked_book_id = :bookId";
+    const values = { userId: authorization.id, bookId };
+
+    const [results] = await connection.query(deleteLikeSql, values);
+    return results;
+};
+
+module.exports = { addLikeToDB, removeLikeFromDB };
